a2c18ecc4e6597d4ff0e45d062c839f240cb010e
Niclas Meyer <niclas270106@gmail.com>
2023-10-24 12:29:36 +0200

    Finished EditArray method

--- FastPy.py
+++ FastPy.py
@@ -2,8 +2,53 @@ import os
 def Load(path):
     pass
 def CreateSaveFile(path):
-    pass
+    base_file_format = ['pdf','png','exe']
+    sort_paths = []
+    open(path,"x")
+    sort_paths = EditArray(sort_paths,'Add_paths','Add path you want to sort like: C:\\Users\\user\\Downloads')
+
 def Save(path,Data):
     pass
-def EditArray(array):
-    pass
+def EditArray(array,title='List_Menu',info=''):
+    while True:
+        print(f'{info}\n__{title}__\n1)Add Element\n2)Remove Element\n3)Save')
+        selection = input('select: ')
+
+        if selection == '1':
+            print('Add Element:')
+            selection = input('Element name: ')
+            array = AvoidDoubleElements(array,selection)
+        elif selection == '2' and len(array)>0:
+            print('Remove Element:')
+            i = 0
+            for x in array:
+                i+=1
+                print(str(i)+') '+str(x))
+            selection = input('Select element: ')
+            #Check for valid number
+            if not selection.isnumeric():
+                continue
+            if int(selection)-1 > len(array) or int(selection)-1 < 0:
+                continue
+            array = RemoveArrayElement(array,array[int(selection)])
+        else:
+            break
+    return array
+
+def Space(lines = 20):
+    while lines > 0:
+        print('\n')
+        lines-=1
+
+def RemoveArrayElement(array,element):
+    new_array = []
+    for x in array:
+        if not x == element:
+            new_array.append(x)
+    return new_array
+def AvoidDoubleElements(array,element):
+    for x in array:
+        if x == element:
+            return array
+    array.append(element)
+    return array
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import FastPy as FP
+
 class MainProgram:
     def __init__(self):
         #Path
@@ -7,14 +8,16 @@ class MainProgram:
         self.user_path = os.path.expanduser("~")
         self.savefile_path = os.path.join(self.user_path, self.file_name)
         self.SortPath = []
+
     def StartupCheck(self):
         #Look for File
         if not os.path.exists(self.savefile_path):
             self.SortPath = FP.CreateSaveFile(self.savefile_path)
+
     def MainLoop(self):
         self.StartupCheck()
         while True:
-            pass
+            print('__Main_Menu__\n')
 
 
 
